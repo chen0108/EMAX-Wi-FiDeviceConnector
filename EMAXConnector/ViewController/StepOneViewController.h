284ebc048e28e9668ff9961b98437ecc41bc3c5f
mbp <Waynn@WynMBP.local>
2018-01-29 13:30:51 +0800

    2

--- EMAXConnector/ViewController/StepOneViewController.h
+++ EMAXConnector/ViewController/StepOneViewController.h
@@ -6,9 +6,23 @@
 //  Copyright © 2018年 emax. All rights reserved.
 //
 
-#import "StepVCHeader.h"
 #import "ConnectorManager.h"
 #import "ConnectorCustomizer.h"
+#import "UIImage+tint.h"
+
+#define kDevice_Is_iPhoneX      ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+#define kStatusH                (kDevice_Is_iPhoneX? 44 : 20)
+#define kNavbarH                44
+#define kSBottom                (kDevice_Is_iPhoneX ? 34 : 0)
+
+#define kNavStatusBarHeigth     (kStatusH+kNavbarH)
+
+#define kScreenWidth            [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight           [UIScreen mainScreen].bounds.size.height
+#define kScreenScale            kScreenWidth / 375 // refer - iPhone6
+
+#define kLocalizedString(Str)   NSLocalizedString(Str, nil)
+
 
 #define kPadding 20
 
