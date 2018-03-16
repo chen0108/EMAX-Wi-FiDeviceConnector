ff5d93571e85aefb7a97a45f1122c39f62c5f8b3
mbp <imhwn@vip.qq.com>
2018-03-16 11:24:07 +0800

    prepare for new features

--- Example/Wi-FiDeviceConnector/ViewController.m
+++ Example/Wi-FiDeviceConnector/ViewController.m
@@ -34,14 +34,15 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
+- (IBAction)newStyleAction {
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
 }
 
 /*
