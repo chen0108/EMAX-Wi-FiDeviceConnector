a1b973ebe9235d7bbf1cd2ee1aefcae9d5db1562
mbp <imhwn@vip.qq.com>
2018-04-03 14:01:02 +0800

    w002 init

--- EMAXConnector/ConnectorManager/W002ConnectorManager.h
+++ EMAXConnector/ConnectorManager/W002ConnectorManager.h
@@ -10,4 +10,39 @@
 
 @interface W002ConnectorManager : BaseConnectorManager
 
+/**
+ 连接设备并准备开始任务链 */
+- (void)connectToDevice:(void(^)(W002ConnectorManager *mgr))didConnectedblock;
+
+/**
+ 连接测试 */
+- (W002ConnectorManager *(^)(void))connectionTest;
+/**
+ 确认测试结果 */
+- (W002ConnectorManager *(^)(void))confirmTestResult;
+/** W002
+ 设置设备工作模式为 APSTA */
+- (W002ConnectorManager *(^)(void))setAPWorkMode;
+/** W002
+ 设置设备连接的服务器地址与端口 */
+- (W002ConnectorManager *(^)(NSString *host, NSString *port))setNETP;
+/**
+ 扫描Wi-Fi */
+- (W002ConnectorManager *(^)(void))scanWiFi;
+/**
+ 设置密码 */
+- (W002ConnectorManager *(^)(NSString *psw, NSString *auth, NSString *encry))setPsw;
+/**
+ 设置SSID */
+- (W002ConnectorManager *(^)(NSString *ssid))setSSID;
+/** W002
+ 查寻STA连接状态 */
+- (W002ConnectorManager *(^)(void))setWSLK;
+/** W002
+ 模块进入透传模式 */
+- (W002ConnectorManager *(^)(void))setENTM;
+/** W002
+ 重启设备 */
+- (W002ConnectorManager *(^)(void))setRebootDevice;
+
 @end
--- EMAXConnector/ConnectorManager/W002ConnectorManager.m
+++ EMAXConnector/ConnectorManager/W002ConnectorManager.m
@@ -8,6 +8,99 @@
 
 #import "W002ConnectorManager.h"
 
+static NSString * const W002Commonds[] = {
+    @"HF-A11ASSISTHREAD",                   // 1 连接测试
+    @"+ok"                                  // 2 确认步骤‘1’返回无误
+    @"AT+WMODE=APSTA\r",                    // 3 设置设备工作模式
+    @"AT+NETP=UDP,CLIENT,%@,%@",            // 4 设置设备服务端端口、地址
+    @"AT+WSCAN",                            // 5 扫描热点
+    @"AT+WSKEY=%@,%@,%@",                   // 6 设置Wi-Fi密码 加密方式
+    @"AT+WSKEY=OPEN,NONE",                  // 6 设置开放的Wi-Fi
+    @"AT+WSSSID=%@",                        // 7 设置SSID
+    @"AT+WSMAC",                            // * 获取设备MAC
+    @"AT+WSLK",                             // 8 查寻STA连接状态
+    @"AT+ENTM",                             // 9 模块进入透传模式
+    @"AT+Z",                                // 10 重启模块
+};
+
+typedef enum : NSUInteger {
+    W002CommondIdx_Test,
+    W002CommondIdx_Confirm,
+    W002CommondIdx_APMode,
+    W002CommondIdx_Net,
+    W002CommondIdx_Scan,
+    W002CommondIdx_Psw,
+    W002CommondIdx_PswOpen,
+    W002CommondIdx_SSID,
+    W002CommondIdx_Mac,
+    W002CommondIdx_WSLK,
+    W002CommondIdx_ENTM,
+    W002CommondIdx_Reboot,
+} W002CommondIdx;
+
+
 @implementation W002ConnectorManager
+
+- (void)connectToDevice:(void(^)(W002ConnectorManager *mgr))didConnectedblock {
+    __weak typeof(self) weakSelf = self;
+    self.didBeginReceiving = ^{ // 连接成功后便开始 接收数据
+        didConnectedblock(weakSelf);
+    };
+    
+    [self initTaskChains];
+    
+    [self.commands addObject:@"connectToDevice"]; // 无实际用途 表示一项任务
+    HandleDataBlock block = ^(NSString *msg){
+        NSLog(@"*=*=Connected block=*=* :%@", msg);
+    };
+    [self.tasks addObject:block];
+    
+    NSError *error = nil;
+    [self.udpSocket enableBroadcast:YES error:&error];
+    [self.udpSocket bindToPort:self.port error:&error];
+    [self.udpSocket receiveOnce:&error];
+    
+    if (error) {
+        NSLog(@"Error connecting: %@", error);
+        self.resultBlock(self, false, self.taskPointer);
+    };
+}
+
+- (W002ConnectorManager *(^)(void))connectionTest {
+    return ^W002ConnectorManager *(void) {
+        [self.commands addObject:W002Commonds[W002CommondIdx_Test]];
+        
+        HandleDataBlock block = ^(NSString *msg){
+            NSLog(@"*=*=%s=*=* :%@", __func__, msg);
+        };
+
+        [self.tasks addObject:block];
+        
+        return self;
+    };
+}
+
+- (W002ConnectorManager *(^)(void))confirmTestResult {
+    return ^W002ConnectorManager *(void) {
+        [self.commands addObject:W002Commonds[W002CommondIdx_Confirm]];
+        
+        
+        return self;
+    };
+}
+
+- (W002ConnectorManager *(^)(void))setAPWorkMode {
+    return ^W002ConnectorManager *(void) {
+        [self.commands addObject:W002Commonds[W002CommondIdx_APMode]];
+        
+        HandleDataBlock block = ^(NSString *msg) {
+            NSLog(@"*=*=%s=*=* :%@", __func__, msg);
+        };
+        
+        [self.tasks addObject:block];
+        
+        return self;
+    };
+}
 
 @end
