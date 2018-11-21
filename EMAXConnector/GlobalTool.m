73cd0aaec116d999a7aaacc36a4a67b61c1440a7
chen0108_mbp <375735289@qq.com>
2018-11-21 09:04:59 +0800

    移除私有api

--- EMAXConnector/GlobalTool.m
+++ EMAXConnector/GlobalTool.m
@@ -12,10 +12,7 @@
 /**
  >>  跳转到wifi设置  */
 + (void)jumpToWifiSettings{
-    ///私有api混淆
-    NSData *encryptString = [[NSData alloc] initWithBytes:(unsigned char []){0x41,0x70,0x70,0x2d,0x50,0x72,0x65,0x66,0x73,0x3a,0x72,0x6f,0x6f,0x74,0x3d,0x57,0x49,0x46,0x49} length:19];
-    NSString *string = [[NSString alloc] initWithData:encryptString encoding:NSUTF8StringEncoding];
-    NSURL *url = [NSURL URLWithString:string];
+    NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
     if ([[UIApplication sharedApplication] canOpenURL:url]) {
         if (@available(iOS 10, *)) {
             [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
